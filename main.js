43ea4e1b1df3dc357a0c6a15218bb951e757efea
Joaquin Franich <joaquinmelo90@gmail.com>
2022-08-17 20:18:54 -0300

    lorem

--- main.js
+++ main.js
@@ -9,3 +9,23 @@ function saludar(nombre) {
 saludar('Pedro');
 saludar('Juan');
 console.log(edad);
+
+let persona = {
+    nombre: "Juan",
+    edad: 29,
+    mascotas: ['perro', 'gato']
+}
+
+persona.sexo = 'M';
+persona.edad = 22;
+persona.mascotas[0] = 'tortuga';
+console.log(persona);
+
+//PROMESAS ---------------------------------
+console.log("antes de promesa...");
+let promise = new Promise(function (resolve, reject) {  
+  setTimeout(() => resolve(), 5000);
+}).then((resp) => {
+  console.log("termino timeout...");
+});
+console.log("despues de promesa...");
