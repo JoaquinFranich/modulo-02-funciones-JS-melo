b108560449ab8971b604f509f2b44d7da5c39941
Joaquin <joaquinmelo90@gmail.com>
2022-08-23 16:18:41 -0300

    lorem ipsum dolor sit.

--- main.js
+++ main.js
@@ -2,30 +2,59 @@ let nombre = "";
 let edad = 13;
 
 function saludar(nombre) {
-    alert('Hola ' + nombre);
-    console.log(edad);
+  alert("Hola " + nombre);
+  console.log(edad);
 }
 
-saludar('Pedro');
-saludar('Juan');
+saludar("Pedro");
+saludar("Juan");
 console.log(edad);
 
 let persona = {
-    nombre: "Juan",
-    edad: 29,
-    mascotas: ['perro', 'gato']
-}
+  nombre: "Juan",
+  edad: 29,
+  mascotas: ["perro", "gato"],
+};
 
-persona.sexo = 'M';
+persona.sexo = "M";
 persona.edad = 22;
-persona.mascotas[0] = 'tortuga';
+persona.mascotas[0] = "tortuga";
 console.log(persona);
 
 //PROMESAS ---------------------------------
 console.log("antes de promesa...");
-let promise = new Promise(function (resolve, reject) {  
+let promise = new Promise(function (resolve, reject) {
   setTimeout(() => resolve(), 5000);
 }).then((resp) => {
   console.log("termino timeout...");
 });
 console.log("despues de promesa...");
+
+//ESTRUCTURA   JSON---------------------------------
+var typePoints = [
+  //key: value
+  {
+    id: 001, //numer
+    label: "String", //string
+    otherLanguage: ["C++", "Java", "Python"], //Array
+    theBest: true, //Boolean
+  },
+  {
+    id: 002, //numer
+    label: "String", //string
+    otherLanguage: ["C++", "Java", "Python"], //Array
+    theBest: true, //Boolean
+  },
+  {
+    id: 003, //numer
+    label: "String", //string
+    otherLanguage: ["C++", "Java", "Python"], //Array
+    theBest: true, //Boolean
+  },
+  {
+    id: 004, //numer
+    label: "String", //string
+    otherLanguage: ["C++", "Java", "Python"], //Array
+    theBest: true, //Boolean
+  },
+];
